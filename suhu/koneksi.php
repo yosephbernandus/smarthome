c9b730a41189916e2986ee9572f32003631b5ae1
Yoseph Bernandus <yosephbernandus@gmail.com>
2020-02-02 23:57:40 +0700

    Update koneksi.php

--- suhu/koneksi.php
+++ suhu/koneksi.php
@@ -3,8 +3,8 @@ date_default_timezone_set('Asia/Makassar');
 
  $host = "localhost";
  $user = "root";
- $pass = "Yoseph_be1509";
- $db   = "tugasakhir";
+ $pass = "";
+ $db   = "smarthome";
  $koneksi = mysqli_connect($host, $user, $pass, $db);
  if(mysqli_connect_errno()){
   echo "Gagal Terhubung ".mysqli_connect_error();
